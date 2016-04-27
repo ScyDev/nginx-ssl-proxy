95393876ec71e53b9cc8fe3ab404f0b0fd393864
ScyDev <lukas.saegesser@gmail.com>
2016-04-27 16:49:15 +0200

    - made port configurable

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /usr/src/docker-registry.conf /tmp/docker-registry.conf
-xyz=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/docker-registry.conf)
+xyz=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<target_port>/${REGISTRY_TARGET_PORT}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/docker-registry.conf)
 echo "$xyz" > /etc/nginx/conf.d/docker-registry.conf
 nginx -g 'daemon off;'
