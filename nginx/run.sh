499f12613a8b2aeec529c744d3da3f068a04ee0a
ScyDev <lukas.saegesser@gmail.com>
2016-05-01 21:00:23 +0200

    - override default server on port 80 to redirect it to HTTPS

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cp /usr/src/docker-registry.conf /tmp/docker-registry.conf
-xyz=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<target_port>/${REGISTRY_TARGET_PORT}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/docker-registry.conf)
-echo "$xyz" > /etc/nginx/conf.d/docker-registry.conf
+abc=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<target_port>/${REGISTRY_TARGET_PORT}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/docker-registry.conf)
+echo "$abc" > /etc/nginx/conf.d/docker-registry.conf
+
+cp /usr/src/default.conf /tmp/default.conf
+xyz=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<target_port>/${REGISTRY_TARGET_PORT}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/default.conf)
+echo "$xyz" > /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
